396558ce03562d194f15a9928be2c363b0dce5d5
Daniel Sanchez Munoz <daniel.sanchez@lemoncode.net>
2020-08-07 15:14:26 +0200

    implement scaffolding

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import { createApp } from './express.server';
+import { envConstants } from './env.constants';
+import { api } from './api';
+
+const app = createApp();
+
+app.use('/api', api);
+
+app.listen(envConstants.PORT, () => {
+  console.log(`Server ready at http://localhost:${envConstants.PORT}/api`);
+});
