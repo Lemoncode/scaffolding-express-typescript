1e8ff8a7eb75c4d24d58c1cb72aa3ff967bf88e6
Luis del Amo <luis@salonkee.lu>
2020-09-15 10:10:00 +0200

    ref:#2 colors package for console messages

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import { createApp } from './express.server';
 import { envConstants } from './env.constants';
 import { api } from './api';
+import colors from 'colors';
 
 const app = createApp();
 
 app.use('/api', api);
 
 app.listen(envConstants.PORT, () => {
-  console.log(`Server ready at http://localhost:${envConstants.PORT}/api`);
+  console.log(
+    `${colors.yellow('Server running in:')}
+    url : http://localhost:${colors.green(envConstants.PORT)}/api
+    mode: ${colors.green(envConstants.NODE_ENV)}`
+  );
 });
