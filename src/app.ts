bb390d17b30d84c2d7a870220dd8922545420455
Braulio Diez Botella <braulio.diez@gmail.com>
2021-01-16 12:05:04 +0100

    base completed

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import { createApp } from './express.server';
 import { envConstants } from './env.constants';
 import { api } from './api';
+import express from 'express';
+import path from 'path';
 
 const app = createApp();
+
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/api', api);
 
